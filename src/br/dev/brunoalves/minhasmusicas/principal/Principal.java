752e0e2a160d1ff98ad346a6bdf6abf46c091ecb
Bruno Alves <basilva@unisuam.edu.br>
2025-07-02 22:20:56 -0300

    initial commit

--- src/br/dev/brunoalves/minhasmusicas/principal/Principal.java
+++ src/br/dev/brunoalves/minhasmusicas/principal/Principal.java
@@ -1,4 +1,38 @@
 package br.dev.brunoalves.minhasmusicas.principal;
 
+import br.dev.brunoalves.minhasmusicas.modelos.MinhasPreferidas;
+import br.dev.brunoalves.minhasmusicas.modelos.Musica;
+import br.dev.brunoalves.minhasmusicas.modelos.Podcast;
+
 public class Principal {
+    public static void main(String[] args) {
+        
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BoalhaDev");
+        meuPodcast.setApresentador("Marcos Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1530; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
 }
